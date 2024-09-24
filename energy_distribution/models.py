aaee0d4977c81ba7563ebc5cd5040591949e61cb
Audrey Macharia <mitchelaudrey@gmail.com>
2024-09-24 12:57:10 +0300

    added models for energy distributions

--- energy_distribution/models.py
+++ energy_distribution/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class EnergyProvider(models.Model):
+    name = models.CharField(max_length=100)
+    location = models.CharField(max_length=100)
+    capacity = models.FloatField()
+
+class EnergyUser(models.Model):
+    name = models.CharField(max_length=100)
+    location = models.CharField(max_length=100)
+    provider = models.ForeignKey(EnergyProvider, on_delete=models.CASCADE)
+    is_off_grid = models.BooleanField(default=False)
+
+class EnergyConsumption(models.Model):
+    user = models.ForeignKey(EnergyUser, on_delete=models.CASCADE)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    amount = models.FloatField()
