45ba064831335e68dbc0171e24b4c24416d70451
Audrey Macharia <mitchelaudrey@gmail.com>
2024-09-24 12:54:44 +0300

    updated dashboard with html and views

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,3 +1,36 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
+from django.contrib.auth.mixins import LoginRequiredMixin
 
-# Create your views here.
+class DashboardView(LoginRequiredMixin, TemplateView):
+    template_name = 'dashboard/dashboard.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        # Add context data for different sections of the dashboard
+        context['energy_distribution'] = self.get_energy_distribution_data()
+        context['payment_plans'] = self.get_payment_plans_data()
+        context['energy_usage'] = self.get_energy_usage_data()
+        context['customer_support'] = self.get_customer_support_data()
+        context['education_campaigns'] = self.get_education_campaigns_data()
+        return context
+
+    def get_energy_distribution_data(self):
+        # Fetch and return energy distribution data
+        pass
+
+    def get_payment_plans_data(self):
+        # Fetch and return payment plans data
+        pass
+
+    def get_energy_usage_data(self):
+        # Fetch and return energy usage data
+        pass
+
+    def get_customer_support_data(self):
+        # Fetch and return customer support data
+        pass
+
+    def get_education_campaigns_data(self):
+        # Fetch and return education campaigns data
+        pass
